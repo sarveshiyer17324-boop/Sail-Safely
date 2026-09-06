765403148fbd3cb61cb729ebfb49248ec7a8ce0e
sarveshiyer17324-boop <sarveshiyer17324@gmail.com>
2026-09-06 10:56:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -238,7 +238,7 @@ function fetchMarineSafety(url) {
 from flask import render_template
 @app.route("/")
 def home():
-return render_template_string(HTML_TEMPLATE)
+   return render_template_string(HTML_TEMPLATE)
 
 @app.route("/get_marine_safety")
 def get_marine_safety():
